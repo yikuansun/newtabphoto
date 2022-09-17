31ab05799d6c2faca061e59ec97062dc85656143
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-09-17 13:22:42 -0400

    image info

--- newtab.js
+++ newtab.js
@@ -1,4 +1,15 @@
 var dayId = `${(new Date()).getMonth() + 1}_${(new Date()).getDate()}_${(new Date()).getFullYear()}`;
-console.log(dayId);
+var rng = new Math.seedrandom(dayId);
 
-document.body.style.backgroundImage = `url(https://picsum.photos/seed/${dayId}/1920/1080)`;
+var photoId = Math.floor(rng() * 1084);
+
+document.body.style.backgroundImage = `url(https://picsum.photos/id/${photoId}/1920/1080)`;
+
+fetch(`https://picsum.photos/id/${photoId}/info`).then(function(res) {
+    res.json().then(function(realRes) {
+        console.log(realRes)
+        document.querySelector("#photoInfo").innerHTML = 
+`Author: ${realRes.author}<br />
+URL: <a href="${realRes.url}">${realRes.url}</a>`;
+    });
+});
